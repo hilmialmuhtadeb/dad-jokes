b8aa4fd592b987bac5b3c5a876ec5457a667155a
Hilmi Almuhtade Billah <hilmialmuhtadeb@gmail.com>
2022-08-04 23:29:09 +0700

    fix loading state

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,15 @@
   </div>
   <div class="mx-auto mb-12 w-4/5 bg-violet-50 rounded-xl shadow-xl text-center px-4 sm:px-8">
     <h1 class="pt-12 text-2xl font-bold">Kasih <span class="text-violet-600 font-serif">jokes bapak-bapak</span> dong 😁</h1>
-    <p class="mt-8 mb-16 font-serif md:text-xl">{{ joke }}</p>
-    <button class="px-8 py-2 rounded-full bg-violet-700 text-violet-50 mb-12" @click="setCurrentJoke">Lagi Dong</button>
+    <div class="mt-8 mb-16">
+      <template v-if="isLoading">
+        <div class="flex justify-center">
+          <img class="animate-spin h-8 w-8 mr-3" viewBox="0 0 24 24" src="../assets/spinner.png" alt="loading-state">
+        </div>
+      </template>
+      <p v-else class="font-serif md:text-xl">{{ joke }}</p>
+    </div>
+    <button class="px-8 py-2 rounded-full bg-violet-700 text-violet-50 mb-12" @click="getOtherJokes">Lagi Dong</button>
   </div>
 </div>
 </template>
@@ -17,6 +24,11 @@ import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
   computed: {
     ...mapGetters({
       joke: 'getCurrentJoke',
@@ -26,6 +38,19 @@ export default {
     ...mapActions({
       setCurrentJoke: 'setCurrentJoke',
     }),
+    getOtherJokes() {
+      this.isLoading = true;
+      this.setCurrentJoke()
+        .then(() => {
+          this.isLoading = false;
+        });
+    },
+  },
+  mounted() {
+    this.setCurrentJoke()
+      .then(() => {
+        this.isLoading = false;
+      });
   },
 };
 </script>
